3f8ad6fc988a4f45f7a14123b056051781a65a5a
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-05-08 14:48:09 +0100

    Progress...

--- src/app/ticket-form/page.tsx
+++ src/app/ticket-form/page.tsx
@@ -1,10 +1,11 @@
-import Background from "./Background";
 import Image from "next/image";
+import Background from "./Background";
+
 
 
 export default function TicketForm(){
   return(
-    <div className="relative w-full p-10">
+    <div className="relative w-full p-10 overflow-hidden">
       <Background />
       <div className="relative z-10 flex items-center justify-center flex-col space-y-10">
         <Image
@@ -14,14 +15,31 @@ export default function TicketForm(){
           height={50}
         />
         <div className="text-center space-y-4 ">
-          <h1 className="text-4xl font-extrabold">Your Journey to Coding Conf <br/>2025 starts here!</h1>
-          <p className="text-sm text-[hsl(245,15%,58%)]">secure your spot at next year's biggest coding conference. </p>
+          <h1 className="text-6xl font-extrabold ">Your Journey to Coding Conf <br/>2025 starts here!</h1>
+          <p className="text-2xl text-[hsl(245,15%,58%)]">secure your spot at next year's biggest coding conference. </p>
         </div>
-        <div className="w-[350px]  justify-start">
-          <p className="text-sm text-[hsl(245,15%,58%)] ">Upload your avatar</p>
-          <div className="w-full h-25 bg-transparent border border-dashed rounded-md" >
-
+        <div className="w-[400px]  justify-start">
+          <p className=" text-[hsl(245,15%,58%)] ">Upload your avatar</p>
+          <div className="w-full h-25 bg-transparent border border-dashed rounded-md flex items-center flex-col p-4" >
+            <div className="bg-[hsl(245,19%,35%)] rounded-md p-1 flex  justify-center">
+              <Image
+                src="/assets/images/icon-upload.svg"
+                alt="Icon Upload"
+                width={30}
+                height={30} />
+            </div>
+            <p>Drag and drop or click to upload</p>
           </div>
+          <div className="flex gap-4">
+            <Image
+              src="/assets/images/icon-info.svg"
+              alt="Icon Info"
+              width={20} 
+             height={20} 
+           />
+           <p className="text-sm">Upload your photos (JPG or PNG. maxsize: 500px )</p>
+         </div>
+        
         </div>
       </div>
     </div>
