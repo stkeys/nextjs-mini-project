7b9de14715aa3a8fe0cf9fa47d590d9f588e5280
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-05-09 05:07:35 +0100

    added changes

--- src/app/ticket-form/Background.tsx
+++ src/app/ticket-form/Background.tsx
@@ -4,13 +4,7 @@ import Image from "next/image";
 const Background = () => {
   return (
     <div>
-      <Image 
-        src="/assets/images/background-desktop.png"
-        alt="Main Background"
-        fill
-        style={{ objectFit: 'cover', zIndex: -1 }} 
-        priority 
-      />
+      
       <Image
         src="/assets/images/pattern-lines.svg"
         alt="Pattern Background"
@@ -24,6 +18,22 @@ const Background = () => {
         width={300}
         height={200}
         className="absolute top-0 right-0 z-0 object-contain"
+        priority 
+      />
+      <Image
+        src="/assets/images/pattern-squiggly-line-top.svg"
+        alt="Pattern Top Background"
+        width={300}
+        height={200}
+        className="absolute top-0 right-0 z-0 object-contain"
+        priority 
+      />
+      <Image
+        src="/assets/images/pattern-squiggly-line-bottom.svg"
+        alt="Pattern Top Background"
+        width={600}
+        height={400}
+        className="absolute bottom-0 z-0 object-contain"
         priority 
       />
     </div>
--- src/app/ticket-form/page.tsx
+++ src/app/ticket-form/page.tsx
@@ -5,9 +5,9 @@ import Background from "./Background";
 
 export default function TicketForm(){
   return(
-    <div className="relative w-full p-10 overflow-hidden">
+    <div className="relative w-full overflow-x-hidden h-screen bg-[url(/assets/images/background-desktop.png)]">
       <Background />
-      <div className="relative z-10 flex items-center justify-center flex-col space-y-10">
+      <div className="relative z-10 flex items-center justify-center flex-col space-y-10 p-10">
         <Image
           src="/assets/images/logo-full.svg"
           alt="Logo"
@@ -16,10 +16,10 @@ export default function TicketForm(){
         />
         <div className="text-center space-y-4 ">
           <h1 className="text-6xl font-extrabold ">Your Journey to Coding Conf <br/>2025 starts here!</h1>
-          <p className="text-2xl text-[hsl(245,15%,58%)]">secure your spot at next year's biggest coding conference. </p>
+          <p className="text-2xl text-[hsl(252,6%,83%)]">secure your spot at next year's biggest coding conference. </p>
         </div>
         <div className="w-[400px]  justify-start">
-          <p className=" text-[hsl(245,15%,58%)] ">Upload your avatar</p>
+          <p className=" text-[hsl(252,6%,83%)] ">Upload your avatar</p>
           <div className="w-full h-25 bg-transparent border border-dashed rounded-md flex items-center flex-col p-4" >
             <div className="bg-[hsl(245,19%,35%)] rounded-md p-1 flex  justify-center">
               <Image
@@ -39,7 +39,9 @@ export default function TicketForm(){
            />
            <p className="text-sm">Upload your photos (JPG or PNG. maxsize: 500px )</p>
          </div>
-        
+         
+
+         
         </div>
       </div>
     </div>
