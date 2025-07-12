44bda29ebf2634989ed996a5938dbc1b7900562f
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-12 04:01:29 +0100

    refactor: replace any[] with ClassValue[] in utils to improve type safety

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: any[]) {
+type ClassValue = string | number | null | boolean | undefined | Record<string, boolean> | ClassValue[];
+
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
