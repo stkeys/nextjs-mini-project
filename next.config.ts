949bfdaf75cfb5de685c2cc5b10eb84ad6c2f433
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-16 15:37:19 +0100

    fix:update next.config.ts back to default

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
