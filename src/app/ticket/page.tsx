001eab3adafd93a70f27544c758231ee454bdf35
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-07-18 16:19:13 +0100

    style: adjust padding in ticket

--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -7,7 +7,7 @@ export default function TicketReceived(){
   return(
     <div className="relative w-full overflow-x-hidden  bg-double text-white">
       <Background />
-      <div className="relative z-10 flex items-center justify-center flex-col space-y-10 p-10 font-500">
+      <div className="relative z-10 flex items-center justify-center flex-col space-y-10 p-2 font-500">
         <Image
           src="/assets/images/logo-full.svg"
           alt="Logo"
@@ -18,7 +18,7 @@ export default function TicketReceived(){
           <h1 className="text-3xl sm:text-5xl font-extrabold ">Congrats, <span className="text-[hsl(7,88%,67%)]">Jonathan Kristof!</span> <br/>Your ticket is ready.</h1>
           <p className="sm:w-[40%] text-sm sm:text-md text-[hsl(252,6%,83%)]">we&apos;ve emailed your ticket to <span className="text-[hsl(7,88%,67%)]">jonatan@gmail.com</span> and will send updates in the run up to the event</p>
         </div>
-        <div className="m-10 relative min-w-[300px] ">
+        <div className=" relative min-w-[100px] ">
           <div className="">
             <Image 
               src="/assets/images/pattern-ticket.svg"
